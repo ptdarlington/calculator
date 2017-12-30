03bead17bb7c29aedeaf85b289b071b947265b95
Paige Darlington <paigetaylordarlington@gmail.com>
2017-12-30 14:55:50 -0800

    implement subtract function

--- index.test.js
+++ index.test.js
@@ -8,10 +8,23 @@ describe('calculator', function() {
       expect(calculator.add(-1, 3)).to.equal(2);
     });
   });
+
+  describe('subtract',function() {
+    it('returns the difference of two numbers', function() {
+      //expect(calculator.subtract(5, 2)).to.equal(3);
+      //expect(calculator.subtract(0, 0)).to.equal(0);
+      expect(calculator.subtract(-1, 3)).to.equal(-4);
+    });
+  });
 });
+
 
 const calculator = {
   add: function(a, b) {
     return a+b;
+  },
+  subtract: function(a, b) {
+    return a-b;
   }
+
 };
